3dfc34f8b0221e0fdb7a18121eafbf5c51ad5760
Lauro Omatsu <lauroomatsu@gmail.com>
2022-06-22 04:07:09 -0300

    update knexfile

--- src/config/knexfile.ts
+++ src/config/knexfile.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { DB_HOST, DB_PORT, POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USER } from '.'
 
 module.exports = {
@@ -6,29 +5,13 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: DB_HOST, 
+      host: DB_HOST,
       database: POSTGRES_DB,
-      user: POSTGRES_USER, 
+      user: POSTGRES_USER,
       password: POSTGRES_PASSWORD,
       ssl: {
         rejectUnauthorized: false,
-    }
-=======
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
- module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
->>>>>>> main
+      }
     },
     migrations: {
       directory: "../database/migrations",
@@ -39,7 +22,6 @@ module.exports = {
   },
 
   staging: {
-<<<<<<< HEAD
     client: 'pg',
     connection: {
       database: 'my_db',
@@ -71,40 +53,4 @@ module.exports = {
     }
   }
 
-=======
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "../database/migrations",
-    },
-    seeds: {},
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "../database/migrations",
-    },
-    seeds: {
-      directory: "./src/database/seeds",
-    },
-  },
->>>>>>> main
 };
