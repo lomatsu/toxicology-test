a3ced3841824c532fac7ef02275755978cbf6cd5
Lauro Omatsu <lauroomatsu@gmail.com>
2022-06-22 02:51:42 -0300

    add knexfile and migration

---
+++ src/config/knexfile.ts
@@ -0,0 +1,46 @@
+import {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER} from '.'
+
+const connection = {
+  DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
+}
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
